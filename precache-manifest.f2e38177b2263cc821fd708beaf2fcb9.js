2252521f8e0bf42dc42840e39823c8845c9a76fc
DJ Satoda <dj.satoda@gmail.com>
2021-08-02 23:13:34 -0700

    Updates

---
+++ precache-manifest.f2e38177b2263cc821fd708beaf2fcb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc3b8777475ec112452e",
+    "url": "/blackcoverbook/static/css/main.a870eb77.chunk.css"
+  },
+  {
+    "revision": "dc3b8777475ec112452e",
+    "url": "/blackcoverbook/static/js/main.dc3b8777.chunk.js"
+  },
+  {
+    "revision": "6a999666a700a4acf780",
+    "url": "/blackcoverbook/static/css/1.5b255aec.chunk.css"
+  },
+  {
+    "revision": "6a999666a700a4acf780",
+    "url": "/blackcoverbook/static/js/1.6a999666.chunk.js"
+  },
+  {
+    "revision": "7c766aae09cc3031c940",
+    "url": "/blackcoverbook/static/js/runtime~main.7c766aae.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/blackcoverbook/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "532cb6618ca1b24b8f9bd223e3183609",
+    "url": "/blackcoverbook/index.html"
+  }
+];
